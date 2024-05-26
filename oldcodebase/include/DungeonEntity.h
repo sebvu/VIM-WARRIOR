98c8f8c04477c23c05ba22255d9e3980f48badda
sebvu <santosjester738@gmail.com>
2024-05-25 21:36:35 -0500

    Add oldcodebase/ to .gitignore

--- oldcodebase/include/DungeonEntity.h
+++
@@ -1,8 +0,0 @@
-#ifndef INCLUDE_DUNGEONENTITY_H_
-#define INCLUDE_DUNGEONENTITY_H_
-
-#include "EnemyEntity.h"
-
-class DungeonEntity : EnemyEntity {};
-
-#endif  // INCLUDE_DUNGEONENTITY_H_
--- oldcodebase/src/PlayerEntity.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/PlayerEntity.h"
-
-#include <iostream>
-
-void PlayerEntity::displayCurrentExperience() {
-    // will be scaled amongst a factor of 20 level
-}
-
-void PlayerEntity::displayCurrentHealth() {}
