543f116de8ac45f0f4aa0da5eb8fe3ed48433925
Noby707 <24847689+Noby707@users.noreply.github.com>
2024-05-30 00:57:25 -0400

    Getting a window up

---
+++ ncursesDay1/hello-world.cpp
@@ -0,0 +1,18 @@
+#include <curses.h>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    // Call the initiate screen routing
+    initscr();			/* Start curses mode 		  */
+	printw("Hello World !!!");	/* Print Hello World		  */
+	refresh();			/* Print it on to the real screen */
+	
+    // lets us see the screen before it closes faster than lightning
+    getch();			/* Wait for user input */
+	endwin();			/* End curses mode		  */
+
+    return 0;
+}
